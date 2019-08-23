728ebd43e808dc110664cf3e7d36ee3ec044be6e
Maxim <lippeveld.maxim@gmail.com>
2019-08-23 17:06:13 +0200

    Aug not working

--- data/sets.py
+++ data/sets.py
@@ -15,17 +15,27 @@ class HDF5Dataset(Dataset):
             else:
                 raise ValueError("Channel %d not present in data-file." % channel)
 
-        self.augmentation = augmentation
+        if augmentation is None:
+            self.getitem = self.getitem
+        else:
+            shape = list(self.handle[self.channels[0]]["images"].shape[1:])
+            shape = [len(self.channels)] + shape
+            self.augmentation = augmentation(shape)
+            self.getitem = self.getitem_withaug
 
     def __len__(self):
         return len(self.handle[self.channels[0]]["images"])
 
     def __getitem__(self, idx):
+        self.getitem(idx)
+
+    def getitem(self, idx):
         im = numpy.array([self.handle[channel]["images"][idx] for channel in self.channels], dtype=numpy.float64)
         masks = numpy.array([self.handle[channel]["masks"][idx] for channel in self.channels], dtype=numpy.float64)
+        return im*masks
 
-        if self.augmentation is None:
-            return im*masks
-        else:
-            return self.augmentation(im*masks)
+    def getitem_withaug(self, idx):
+        im = numpy.array([self.handle[channel]["images"][idx] for channel in self.channels], dtype=numpy.float64)
+        masks = numpy.array([self.handle[channel]["masks"][idx] for channel in self.channels], dtype=numpy.float64)
+        return self.augmentation(im*masks)
     
--- func/augtest.py
+++ func/augtest.py
@@ -1,8 +1,9 @@
 from data import sets
 from torch.utils.data import DataLoader
+from util.augmentation import augmentation_2d
 
 def main(args):
-    ds = sets.HDF5Dataset(args.data, args.channels)
+    ds = sets.HDF5Dataset(args.data, args.channels, augmentation=augmentation_2d.Rotate90)
     loader = DataLoader(ds, batch_size=32, shuffle=True, drop_last=True, pin_memory=True, num_workers=8)
 
     for i_batch, sample_batched in enumerate(loader):
