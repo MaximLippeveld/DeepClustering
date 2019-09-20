8d82f5b8d33301761fa9ffa7b1cd05d1f83aa19d
Maxim <lippeveld.maxim@gmail.com>
2019-09-20 11:07:52 +0200

    Further implement CAE

--- func/fit_cae.py
+++ func/fit_cae.py
@@ -14,32 +14,35 @@ def main(args):
     # prepare data
     if type(args.data) is Path:
         ds = sets.HDF5Dataset(args.data, args.channels)
-        shape = ds.get_shape()[1:]
+        img_shape = ds.get_shape()[1:]
+        channel_shape = ds.get_shape()[2:]
         pre_augs = [ToTensor(cuda=args.cuda)]
         post_augs = []
     else:
         ds = datasets.FashionMNIST("data/datasets/", train=True, download=True).data
-        shape = tuple(ds.shape[1:])
-        pre_augs = [Stack(cuda=args.cuda), Unsqueeze(cuda=args.cuda)]
+        channel_shape = ds.shape[1:]
+        ds = torch.unsqueeze(ds, 1)
+        img_shape = ds.shape[1:]
+        pre_augs = [Stack(cuda=args.cuda)]
         post_augs = []
 
     augs = pre_augs + [
         ToFloatTensor(cuda=args.cuda),
-        FlipX(shape, cuda=args.cuda),
-        FlipY(shape, cuda=args.cuda),
-        RandomDeformation(shape, sampling_interval=7, cuda=args.cuda),
-        RotateRandom(shape, cuda=args.cuda)
+        FlipX(channel_shape, cuda=args.cuda),
+        FlipY(channel_shape, cuda=args.cuda),
+        RandomDeformation(channel_shape, sampling_interval=7, cuda=args.cuda),
+        RotateRandom(channel_shape, cuda=args.cuda)
     ] + post_augs
     augmenter = transforms.Compose(augs)
 
     loader_aug = DataLoader(
         ds, batch_size=args.batch_size, shuffle=False, 
-        drop_last=False, num_workers=5,
+        drop_last=False, num_workers=0,
         collate_fn=augmenter
     )
 
     # get model, optimizer, loss
-    cae = model.cae.ConvolutionalAutoEncoder(shape, 10)
+    cae = model.cae.ConvolutionalAutoEncoder(img_shape, 10)
 
     if args.cuda:
         cae.cuda()
--- model/cae.py
+++ model/cae.py
@@ -7,8 +7,9 @@ http://openaccess.thecvf.com/content_ICCV_2017/papers/Dizaji_Deep_Clustering_via
 
 import torch
 import torch.nn as nn
+import numpy
 
-class ConvolutionalEncoder:
+class ConvolutionalEncoder(nn.Module):
 
     """Convolutional encoder
     """
@@ -23,15 +24,15 @@ class ConvolutionalEncoder:
         super(ConvolutionalEncoder, self).__init__()
 
         filters = [32, 16]
-        embedding_input_shape = ((input_shape[-1]//2)//2)*filters[-1]
+        embedding_input_shape = ((input_shape[-1]//2)//2)*((input_shape[-2]//2)//2)*filters[-1]
 
         self.layers = nn.ModuleList()
 
         self.layers.extend([
-            nn.Conv2d(input_shape[0], filters[0], stride=2, kernel_size=5),
-            nn.LeakyReLu(),
-            nn.Conv2d(filters[0], filters[1], stride=2, kernel_size=5),
-            nn.LeakyReLu(),
+            nn.Conv2d(input_shape[0], filters[0], stride=2, kernel_size=5, padding=2),
+            nn.LeakyReLU(),
+            nn.Conv2d(filters[0], filters[1], stride=2, kernel_size=5, padding=2),
+            nn.LeakyReLU(),
             nn.Flatten(),
             nn.Linear(embedding_input_shape, embedding_shape),
             nn.Tanh()
@@ -53,18 +54,21 @@ class ConvolutionalDecoder(nn.Module):
         
         filters = [16, 32]
 
-        self.reconstruction_shape = reconstruction_shape
-        shape = ((reconstruction_shape[-1]//2)//2)*filters[0]
+        self.shape = (
+            filters[0],
+            reconstruction_shape[-1]//2//2,
+            reconstruction_shape[-2]//2//2,
+        )
         
-        self.linear = nn.Linear(input_shape, shape)
+        self.linear = nn.Linear(input_shape, numpy.prod(self.shape))
         self.tanh = nn.Tanh()
         self.layers = nn.ModuleList()
 
         self.layers.extend([
-            nn.ConvTranspose2d(filters[0], filters[1], stride=2, kernel_size=5),
-            nn.LeakyReLu(),
-            nn.ConvTranspose2d(filters[1], reconstruction_shape[0], stride=2, kernel_size=5),
-            nn.LeakyReLu()
+            nn.ConvTranspose2d(filters[0], filters[1], stride=2, kernel_size=5, padding=2),
+            nn.LeakyReLU(),
+            nn.ConvTranspose2d(filters[1], reconstruction_shape[0], stride=2, kernel_size=5, padding=2),
+            nn.LeakyReLU()
         ])
 
 
@@ -72,13 +76,13 @@ class ConvolutionalDecoder(nn.Module):
 
         x = self.linear(x)
         x = self.tanh(x)
-        x = torch.reshape(x, self.reconstruction_shape)
+        x = torch.reshape(x, [-1] + list(self.shape))
 
         for layer in self.layers:
             x = layer(x)
         return x
 
-class ConvolutionalAutoEncoder:
+class ConvolutionalAutoEncoder(nn.Module):
 
     def __init__(self, input_shape, embedding_shape):
 
